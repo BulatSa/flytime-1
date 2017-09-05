403c4d124a2a3b1b85a4f6b00e381d171b9a6a56
Andrew <andreiduffy@gmail.com>
2017-09-05 15:20:31 +0300

    Initial commit

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+
+	<title>Время летать</title>
+
+	<!-- og -->
+	<meta property="og:title" content="Время летать">
+	<meta property="og:description" content="Время летать">
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="/img/og.jpg">
+	<!-- og -->
+
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu:400,700&amp;subset=cyrillic" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+<div class="s-top-panel">
+	<div class="container">
+		
+		<nav class="top-nav">
+			<a href="#">О нас</a>
+			<a href="#" class="active">Доставка</a>
+			<a href="#">Оплата</a>
+			<a href="#">Гарантия качества</a>
+			<a href="#">Отзывы</a>
+			<a href="#">Акции</a>
+			<a href="#">Бизнесу</a>
+			<a href="#">Контакты</a>
+		</nav>
+		
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,25 @@
+﻿<?php include('header.php'); ?>
+
+<section>
+	<div class="container">
+		<h1>Отзывы наших клиентов Отзывы наших клиентов Отзывы наших клиентов Отзывы наших клиентов Отзывы наших клиентов Отзывы наших клиентов </h1>
+		<h2>Hello from the other side</h2>
+		<h3>Hello from the other side</h3>
+		<p>Экстренная помощь MIG</p>
+		<ul>
+			<li>Экстренная помощь MIG</li>
+			<li>Экстренная помощь MIG</li>
+			<li>Экстренная помощь MIG</li>
+		</ul>
+
+		<a href="#" class="fancy btn" data-src="#modal-order-1">Открыть order</a>
+		<a href="#" class="fancy btn" data-src="#modal-order-2">Открыть order 2</a>
+		<a href="img/img.png" class="fancy btn">Открыть img</a>
+
+
+
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
