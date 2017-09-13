872d5173778935ec5562f4ab5569700cbfcecbbc
Bulat <sadikovba@gmail.com>
2017-09-13 11:44:26 +0300

    Add About, .user-content

---
+++ about.php
@@ -0,0 +1,83 @@
+<?php include('header.php'); ?>
+
+<section class="s-intro">
+	<div class="intro-wrapper">
+		<div class="intro intro--about">
+			<div class="intro__content">
+				<span class="h1">О нас</span>
+				<div class="user-content">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.</p>
+				</div>
+				<div class="intro__facts">
+					<div class="intro__fact">
+						<span class="h1">8 лет</span>
+						<p>опыта на рынке</p>
+					</div>
+					<div class="intro__fact">
+						<span class="h1">1 млн.</span>
+						<p>надутых шаров</p>
+					</div>
+					<div class="intro__fact">
+						<span class="h1">90%</span>
+						<p>клиентов возвращаются</p>
+					</div>
+				</div>
+				<div class="intro__sign">
+					<span class="h3">Акопян Артур</span>
+					<p>Генеральный директор</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="about-info-sec">
+	<div class="container">
+		<div class="about-info">
+			<div class="about-info__text-wrap">
+				<p class="about-info__title h1">Любой праздник на высоте</p>
+				<div class="row">
+					<div class="grid-6 grid-12_s">
+						<div class="user-content about-info__text">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur cum debitis facere harum illo minus nihil nulla qui quia quos reiciendis sapiente sed, temporibus unde vel velit, veritatis vero?</p>
+						</div>
+					</div>
+					<div class="grid-6 grid-12_s">
+						<div class="user-content about-info__text">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur cum debitis facere harum illo minus nihil nulla qui quia quos reiciendis sapiente sed, temporibus unde vel velit, veritatis vero?</p>
+						</div>
+					</div>
+					<div class="grid-12">
+						<img class="about-info__baloons" src="img/about/info-baloons.jpg" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="about-info__text-wrap about-info__text-wrap--individual">
+				<div class="row">
+					<div class="grid-6 grid-12_s">
+						<p class="about-info__title h1">Индивидуальный подход
+						</p>
+						<div class="user-content about-info__text">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur cum debitis facere harum illo minus nihil nulla qui quia quos reiciendis sapiente sed, temporibus unde vel velit, veritatis vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid, at culpa cumque facere nam rem saepe! Consequatur debitis ea eveniet explicabo, facere magni minima? Aperiam eligendi perspiciatis ratione soluta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae consequatur dolore ea eius et incidunt, laboriosam odio quibusdam tempora totam voluptates. Aperiam earum esse itaque nesciunt praesentium, quas vitae? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus excepturi fugiat labore necessitatibus quidem ut? Aliquam amet corporis cumque cupiditate dolor expedita, nam odio perferendis possimus quasi rem repudiandae, voluptates!</p>
+							<ul>
+								<li>Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus; </li>
+								<li>Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus; </li>
+								<li>Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus; </li>
+							</ul>
+						</div>
+						<div class="about-info__sign">
+							<span class="h3">Мария Воздушная</span>
+							<p>Руководитель по работе с клиентами</p>
+						</div>
+					</div>
+					<div class="grid-6 grid-12_s">
+						<img class="about-info__girl" src="img/about/info-girl.jpg" alt="">
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
