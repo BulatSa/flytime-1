93338d5896eb9e7ec691bc733176cb155885405c
Bulat <sadikovba@gmail.com>
2017-09-14 12:03:14 +0300

    Add Delivery

---
+++ delivery.php
@@ -0,0 +1,92 @@
+<?php include('header.php'); ?>
+
+<section class="s-intro">
+		<div class="intro-wrapper">
+			<div class="intro intro--delivery">
+				<div class="intro__content">
+					<span class="h1">Доставляем быстро и бесплатно*</span>
+					<div class="user-content">
+						<p>Обращаем ваше внимание на то, что доставка гелевых шаров производится ежедневно и круглосуточно. Для доставки вы сможете выбрать любое удобное время с интервалом 2-3 часа. Мы сделаем все возможное, чтобы привезти шарики вовремя. </p>
+					</div>
+					<div class="intro__delivery-list">
+						<div class="intro__delivery-item">
+							<div class="intro__delivery-item-img">
+								<img src="img/svg/intro-2-bubbles.svg" alt="">
+							</div>
+							<span>Доставка шариков в&nbsp;течение 2-х часов</span>
+						</div>
+						<div class="intro__delivery-item">
+							<div class="intro__delivery-item-img">
+								<img src="img/svg/intro-bus.svg" alt="">
+							</div>
+							<span>Круглосуточная доставка</span>
+						</div>
+						<div class="intro__delivery-item">
+							<div class="intro__delivery-item-img">
+								<img src="img/svg/intro-bubble.svg" alt="">
+							</div>
+							<span>Возможен самовывоз</span>
+						</div>
+					</div>
+					<span class="intro__delivery-descr">* Бесплатная доставка товара осуществляется в пределах МКАД</span>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<section class="about-info-sec">
+		<div class="container">
+			<div class="about-info">
+				<div class="about-info__text-wrap">
+					<div class="row">
+						<div class="grid-6 grid-12_s">
+							<p class="about-info__title h2">Самовывоз</p>
+							<div class="user-content about-info__text">
+								<p>Вы можете забрать заказ самостоятельно в нашем магазине — шоу-руме. Услуга самовывоза — бесплатна. Если заказанный Вами товар есть в наличии в магазине, Вы сможете забрать товар в тот же день.</p>
+								<p>Если товар необходимо доставить со склада, то Вы сможете забрать заказ в течение нескольких дней, в зависимости от времени поступления заказа. С Вами свяжется менеджер магазина и сообщит о готовности заказа.</p>
+							</div>
+							<a href="#" class="btn btn--big">Адрес самововывоза <i class="i-right"></i></a>
+						</div>
+						<div class="grid-6 grid-12_s">
+							<p class="about-info__title h2">Доставка
+							</p>
+							<div class="user-content about-info__text">
+								<p>Доставка товаров по Москве и Московской области осуществляются по указанному Вами адресу на следующий день или через день после оформления заказа в зависимости от времени поступления заказа. При оформлении заказа до 13:00 доставка осуществляется на следующий день. При оформлении заказа после 13:00 доставка осуществляется через день.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="about-info__delivery-ballons">
+					<div class="row">
+						<div class="grid-3 grid-6_s">
+							<div class="about-info__delivery-ballon">
+								<span class="h5">Доставляем быстро</span>
+								<img src="img/delivery/delivery-ballon-1.png" alt="">
+							</div>
+						</div>
+						<div class="grid-3 grid-6_s">
+							<div class="about-info__delivery-ballon about-info__delivery-ballon--toped">
+								<img src="img/delivery/delivery-ballon-2.png" alt="">
+								<span class="h5">Надуваем на месте, поэтому шары дольше летают</span>
+							</div>
+						</div>
+						<div class="grid-3 grid-6_s">
+							<div class="about-info__delivery-ballon">
+								<span class="h5">Более 400 наименований и типов шаров</span>
+								<img src="img/delivery/delivery-ballon-1.png" alt="">
+							</div>
+						</div>
+						<div class="grid-3 grid-6_s">
+							<div class="about-info__delivery-ballon about-info__delivery-ballon--toped">
+								<img src="img/delivery/delivery-ballon-2.png" alt="">
+								<span class="h5">Скидки и программы лояльности</span>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+<?php include('footer.php'); ?>
