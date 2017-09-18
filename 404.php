f3657536c0046bce190c01f6c7550e68dc113320
Bulat <sadikovba@gmail.com>
2017-09-18 10:49:37 +0300

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php include('header.php'); ?>
+
+<section class="not-found-sec">
+	<div class="container">
+		<div class="not-found">
+			<div class="not-found__img">
+				<img src="img/404/404.jpg" alt="">
+			</div>
+			<div class="not-found__text">
+				<h3>Нам очень жаль, но страницу, которую вы искали не существует</h3>
+			</div>
+			<div class="not-found__links">
+				<a href="#">Главная страница</a>
+				<a href="#">Каталог шаров</a>
+				<a href="#">Контакты</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
