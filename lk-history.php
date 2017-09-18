342a262b5a1be4fdf7e31ae68a5e6a04a28d8a37
Andrew <andreiduffy@gmail.com>
2017-09-18 14:55:53 +0300

    ready: lk-history

---
+++ lk-history.php
@@ -0,0 +1,324 @@
+<?php include('header.php'); ?>
+
+
+<section class="s-page-title">
+	<div class="container">
+		<h1 class="h1">Личный кабинет</h1>
+
+		<nav class="lk-nav">
+			<a href="/lk-profile.php">Профиль</a>
+			<a href="/lk-history.php" class="active">Заказы</a>
+			<a href="/lk-favorite.php">Избранное</a>
+			<a href="#">Выход</a>
+		</nav>
+
+	</div>
+</section>
+
+
+<section class="s-history">
+	<div class="container">
+
+		<div class="row">
+
+			<div class="grid-6 grid-12_m">
+				<div class="history-panel">
+
+					<div class="history-panel__header">
+						<p class="h3">Заказ #123456 от 20.08.2017</p>
+						<div class="history-panel__status">Доставляется</div>
+					</div>
+
+					<div class="checkout-products">
+						<table>
+							<tr>
+								<th>Наименование</th>
+								<th>Цена</th>
+								<th>Кол-во</th>
+								<th>Стоимость</th>
+							</tr>
+							<tr>
+								<td>
+									<span class="h5">Большой прозрачный шар Морской пузырик</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">3 430 руб.</span>
+									<span class="checkout-products__oldprice">3 810 руб.</span>
+								</td>
+								<td>
+									<span class="checkout-products__num">1</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">3 430 руб.</span>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<span class="h5">Большой прозрачный шар</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">344 430 руб.</span>
+									<span class="checkout-products__oldprice">3 810 руб.</span>
+								</td>
+								<td>
+									<span class="checkout-products__num">2</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">688 430 руб.</span>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<span class="h5">Большой прозрачный шар</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">4 430 руб.</span>
+								</td>
+								<td>
+									<span class="checkout-products__num">555</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">250 430 руб.</span>
+								</td>
+							</tr>
+						</table>
+					</div>
+
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Товаров на сумму</div>
+						<div class="checkout-summary__val">14 700 руб.</div>
+					</div>
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Скидка</div>
+						<div class="checkout-summary__val">4 410 руб.</div>
+					</div>
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Доставка</div>
+						<div class="checkout-summary__val">1 100 руб.</div>
+					</div>
+					<div class="checkout-summary checkout-summary--all">
+						<div class="checkout-summary__param">Итого</div>
+						<div class="checkout-summary__val">555 390 руб.</div>
+					</div>
+
+
+
+				</div>
+			</div>
+
+			<div class="grid-6 grid-12_m">
+				<div class="history-panel">
+
+					<div class="history-panel__header">
+						<p class="h3">Заказ #123456 от 20.08.2017</p>
+						<div class="history-panel__status history-panel__status--blue">Выполнен</div>
+					</div>
+
+					<div class="checkout-products">
+						<table>
+							<tr>
+								<th>Наименование</th>
+								<th>Цена</th>
+								<th>Кол-во</th>
+								<th>Стоимость</th>
+							</tr>
+							<tr>
+								<td>
+									<span class="h5">Большой прозрачный шар</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">344 430 руб.</span>
+									<span class="checkout-products__oldprice">3 810 руб.</span>
+								</td>
+								<td>
+									<span class="checkout-products__num">2</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">688 430 руб.</span>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<span class="h5">Большой прозрачный шар</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">4 430 руб.</span>
+								</td>
+								<td>
+									<span class="checkout-products__num">555</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">250 430 руб.</span>
+								</td>
+							</tr>
+						</table>
+					</div>
+
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Товаров на сумму</div>
+						<div class="checkout-summary__val">14 700 руб.</div>
+					</div>
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Скидка</div>
+						<div class="checkout-summary__val">4 410 руб.</div>
+					</div>
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Доставка</div>
+						<div class="checkout-summary__val">1 100 руб.</div>
+					</div>
+					<div class="checkout-summary checkout-summary--all">
+						<div class="checkout-summary__param">Итого</div>
+						<div class="checkout-summary__val">555 390 руб.</div>
+					</div>
+
+					<div class="history-panel__actions">
+						<button type="submit" class="btn btn--big btn--outline">Повторить заказ <i class="i-right"></i></button>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="grid-6 grid-12_m">
+				<div class="history-panel">
+
+					<div class="history-panel__header">
+						<p class="h3">Заказ #123456 от 20.08.2017</p>
+						<div class="history-panel__status history-panel__status--blue">Выполнен</div>
+					</div>
+
+					<div class="checkout-products">
+						<table>
+							<tr>
+								<th>Наименование</th>
+								<th>Цена</th>
+								<th>Кол-во</th>
+								<th>Стоимость</th>
+							</tr>
+							<tr>
+								<td>
+									<span class="h5">Большой прозрачный шар</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">344 430 руб.</span>
+									<span class="checkout-products__oldprice">3 810 руб.</span>
+								</td>
+								<td>
+									<span class="checkout-products__num">2</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">688 430 руб.</span>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<span class="h5">Большой прозрачный шар</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">4 430 руб.</span>
+								</td>
+								<td>
+									<span class="checkout-products__num">555</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">250 430 руб.</span>
+								</td>
+							</tr>
+						</table>
+					</div>
+
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Товаров на сумму</div>
+						<div class="checkout-summary__val">14 700 руб.</div>
+					</div>
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Скидка</div>
+						<div class="checkout-summary__val">4 410 руб.</div>
+					</div>
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Доставка</div>
+						<div class="checkout-summary__val">1 100 руб.</div>
+					</div>
+					<div class="checkout-summary checkout-summary--all">
+						<div class="checkout-summary__param">Итого</div>
+						<div class="checkout-summary__val">555 390 руб.</div>
+					</div>
+
+					<div class="history-panel__actions">
+						<button type="submit" class="btn btn--big btn--outline">Повторить заказ <i class="i-right"></i></button>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="grid-6 grid-12_m">
+				<div class="history-panel">
+
+					<div class="history-panel__header">
+						<p class="h3">Заказ #123456 от 20.08.2017</p>
+						<div class="history-panel__status history-panel__status--blue">Выполнен</div>
+					</div>
+
+					<div class="checkout-products">
+						<table>
+							<tr>
+								<th>Наименование</th>
+								<th>Цена</th>
+								<th>Кол-во</th>
+								<th>Стоимость</th>
+							</tr>
+							<tr>
+								<td>
+									<span class="h5">Большой прозрачный шар</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">4 430 руб.</span>
+								</td>
+								<td>
+									<span class="checkout-products__num">555</span>
+								</td>
+								<td>
+									<span class="checkout-products__price">250 430 руб.</span>
+								</td>
+							</tr>
+						</table>
+					</div>
+
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Товаров на сумму</div>
+						<div class="checkout-summary__val">14 700 руб.</div>
+					</div>
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Скидка</div>
+						<div class="checkout-summary__val">4 410 руб.</div>
+					</div>
+					<div class="checkout-summary">
+						<div class="checkout-summary__param">Доставка</div>
+						<div class="checkout-summary__val">1 100 руб.</div>
+					</div>
+					<div class="checkout-summary checkout-summary--all">
+						<div class="checkout-summary__param">Итого</div>
+						<div class="checkout-summary__val">555 390 руб.</div>
+					</div>
+
+					<div class="history-panel__actions">
+						<button type="submit" class="btn btn--big btn--outline">Повторить заказ <i class="i-right"></i></button>
+					</div>
+
+				</div>
+			</div>
+
+		</div>
+
+		<div class="pagination">
+			<a href="#" title="Предыдущая страница"><i class="i-left"></i></a>
+			<a href="#" class="active">1</a>
+			<a href="#">2</a>
+			<a href="#">3</a>
+			<a href="#" title="Следующая страница"><i class="i-right"></i></a>
+		</div>
+
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
